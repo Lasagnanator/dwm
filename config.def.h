f3a43fe6587ae7775df935b51c99c37a6325b7b8
Lasagnanator <d96a07@gmail.com>
2022-08-22 12:10:10 +0000

    Added cfacts-floating/centerdmaster

--- config.def.h
+++ config.def.h
@@ -42,7 +42,9 @@ static const Layout layouts[] = {
     { "[]=",      tile },    /* first entry is default */
     { "><>",      NULL },    /* no layout function means floating behavior */
     { "[M]",      monocle },
-	{ "[D]",      deck },
+    { "[D]",      deck },
+    { "|M|",      centeredmaster },
+    { ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -84,10 +86,12 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_d,      setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                       XK_e,      setlayout,      {.v = &layouts[4]} },
+//  { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
     { MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
