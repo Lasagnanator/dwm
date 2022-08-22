cd475737b40f3499676d4c3b3c00331455f90ece
Lasagnanator <d96a07@gmail.com>
2022-08-22 16:16:11 +0000

    Added vanitygaps function, removed all the layout files added in the previous patch since I probably am not able to read

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -38,10 +43,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1
-#include "deck.c"
-#include "centeredmaster.c"
-#include "bstack.c"
-#include "nrowgrid.c"
+#include "vanitygaps.c"
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -101,6 +103,7 @@ static Key keys[] = {
     { MODKEY,                       XK_r,      setlayout,      {.v = &layouts[5]} },
     { MODKEY,                       XK_n,      setlayout,      {.v = &layouts[6]} },
     { MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
+	{ MODKEY,                       XK_g,      togglegaps,     {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
     { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
